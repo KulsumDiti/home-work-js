adea78ecf5866876c6de7a0bab9e2a221d53d634
KulsumDiti <kulsumditi3@gmail.com>
2025-01-26 20:40:56 +0600

    file added

---
+++ New folder/if-else.js
@@ -0,0 +1,45 @@
+
+
+let months = +prompt();
+
+if(months ==1){
+    document.write('January');
+}
+
+else if(months ==2){
+    document.write('February');
+}
+else if(months ==3){
+    document.write('March');
+}
+else if(months ==4){
+    document.write('April');
+}
+else if(months ==5){
+    document.write('May');
+}
+else if(months ==6){
+    document.write('June');
+}
+else if(months ==7){
+    document.write('July');
+}
+else if(months ==8){
+    document.write('August');
+}
+else if(months ==9){
+    document.write('September');
+}
+else if(months ==10){
+    document.write('October');
+}
+else if(months ==11){
+    document.write('November');
+}
+else if(months ==12){
+    document.write('December');
+}
+
+else{
+    document.write('Invailed Number');
+}
